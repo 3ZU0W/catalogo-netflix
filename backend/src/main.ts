ac065d2fc7ac833034728525aae9a15713759077
monze <monserratmendozaduran@gmail.com>
2026-06-06 23:33:36 -0400

    agregar vercel al CORS

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -9,7 +9,7 @@ async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:5173',
+    origin: ['http://localhost:5173', 'https://catalogo-netflix-theta.vercel.app'],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
   });
